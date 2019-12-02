67b01ef4938441d1f1c22564c4cc3f2790f1a7c0
Daniel Tan <dtan@tripadvisor.com>
2019-12-02 09:51:23 +0800

    day1

---
+++ 01a.py
@@ -0,0 +1,51 @@
+#%%
+import math
+
+#Fuel required to launch a given module is based on its mass. 
+# Specifically, to find the fuel required for a module, take its mass, divide by three, 
+# round down, and subtract 2.
+
+#%%
+def fuel(mass):
+    return math.floor(mass/3)-2
+
+# %%
+# For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
+# For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
+# For a mass of 1969, the fuel required is 654.
+# For a mass of 100756, the fuel required is 33583.
+
+#%%
+assert fuel(12)==2
+assert fuel(14)==2
+assert fuel(1969)==654
+assert fuel(100756)==33583
+
+#%%
+data = [line.rstrip() for line in open('data/01.dat')]
+
+# %%
+ans_a=0
+for i in data:
+    ans_a+=fuel(int(i))
+print(ans_a)
+# %%
+
+## Part B: Recursion
+
+#%%
+def fuel_recursion(mass):
+    if mass<=0:
+        return mass
+    else:
+        return fuel_recursion(fuel_recursion(mass))
+
+# %%
+
+ans_b=0
+for i in data:
+    f=fuel(int(i))
+    while f>0:
+        ans_b+=f
+        f=fuel(f)
+print(ans_b)
